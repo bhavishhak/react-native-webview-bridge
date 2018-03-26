532abedf24545aef15034789f95099bb98e95712
bhavish <bhavish@geekyants.com>
2018-03-26 11:35:34 +0530

    changed react create class

--- webview-bridge/index.android.js
+++ webview-bridge/index.android.js
@@ -17,6 +17,8 @@ var React = require('react');
 var ReactNative = require('react-native');
 var invariant = require('invariant');
 var keyMirror = require('keymirror');
+var createReactClass = require('create-react-class');
+
 var resolveAssetSource = require('react-native/Libraries/Image/resolveAssetSource');
 
 var {
@@ -48,7 +50,7 @@ var RCTWebViewBridge = requireNativeComponent('RCTWebViewBridge', WebViewBridge)
 /**
  * Renders a native WebView.
  */
-var WebViewBridge = React.createClass({
+var WebViewBridge = createReactClass({
 
   propTypes: {
     ...RCTWebViewBridge.propTypes,
